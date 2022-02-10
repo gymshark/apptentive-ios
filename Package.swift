e1d1eda6f373c8f415c903ce1dec30c412875812
Sergey Bolshedvorsky <s.bolshedvorsky@gymshark.com>
2022-02-10 11:57:48 +0000

    Add SPM Support for version 5.3.4

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Apptentive",
+    platforms: [ .iOS(.v13) ],
+    products: [
+        .library(
+            name: "Apptentive",
+            targets: ["Apptentive"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "Apptentive",
+            path: "./Apptentive.5.3.4.xcframework"
+        )
+    ]
+)
